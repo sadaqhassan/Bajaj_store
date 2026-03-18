57fbf033bcf5825ecd43892b3291d18cc3d32a30
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-18 06:41:09 +0300

    currentUser

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { MenuIcon } from 'lucide-react'
 
 const Nav = () => {
+  const [currentUser,setCurrentUser] = useState(null)
   const [menuOpen , setMenuOpen] = useState(false) 
   return (
     <div>
@@ -14,20 +15,24 @@ const Nav = () => {
           <Link to={'/'}> Bajajs </Link>
           <Link to={'/'}> My Lists </Link>
         </div>
-        <div className='relative group'>
+        {
+          currentUser  || currentUser != null ?
+          <div className='relative group'>
           <img src="./user.png" alt="" className='w-10 h-10 rounded-full'/>
           <ul className='hidden z-10 text-gray-900 px-4 rounded py-2 fixed  right-8  top-14 w-30 group-hover:block bg-white shadow-xl '>
             <li className='cursor-pointer pb-3'>Logout</li>
             <li className='cursor-pointer '>Profile</li>
           </ul>
-        </div>
+        </div> :
+        <button className='bg-cyan-500 px-2 py-1 rounded'>Login</button>        
+        }
       </div>
 
       {/* ...mobile... */}
       <div className='flex justify-between px-4 md:hidden py-4 text-white bg-gray-700'>
         <p>Bajaj_Store</p>
         <div className='flex space-x-4'>
-            <button>Logout</button>
+            <button className='bg-cyan-500 px-2 py-1 rounded'>Login</button>
             <button onClick={()=>setMenuOpen(!menuOpen)}> <MenuIcon/></button>
         </div>
       </div>
