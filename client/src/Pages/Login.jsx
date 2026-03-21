425f383e8f3731c2afc56b3d0a1065a269595a9b
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-21 14:10:50 +0300

    Turned dynamic State

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,16 +1,42 @@
 import React, { useState } from 'react'
 
 const Login = () => {
-  return (
-    <div className='flex flex-col justify-center items-center fixed inset-0 left-0 right-0 bottom-0 top-0 '>
+    const [state,setState] = useState("login");
 
-        <form className='p-10 rounded bg-white shadow-2xl flex flex-col space-y-4' action="">
-            <h1 className='text-center text-2xl font-medium mb-4'>Login</h1>
-            <input type="text" placeholder='username' name='username' className='bg-gray-100 px-2 py-1 rounded text-black'/>
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+    }
+
+return (
+    <div className='flex flex-col justify-center items-center fixed inset-0 left-0 right-0 bottom-0 top-0 '>
+        <form onSubmit={handleSubmit} className='p-10 rounded bg-white shadow-2xl flex flex-col space-y-4' action="">
+            <h1 className='text-center text-2xl font-medium mb-4'>{
+                state === "login" ? "Login" : "Register"
+            }</h1>
+            {
+                state === "register" &&
+                <input type="text" placeholder='username' name='username' className='bg-gray-100 px-2 py-1 rounded text-black'/>
+            }
             <input type="email" placeholder='email' name='email' className='bg-gray-100 px-2 py-1 rounded text-black'/>
             <input type="password" placeholder='password' name='password' className='bg-gray-100 px-2 py-1 rounded text-black'/>
-            <input className='bg-cyan-600 text-white py-1' type="submit" value="Login" />
+            <input className='bg-cyan-600 text-white py-1' type="submit" value={state === "login" ? "Login" : "Register"} />
+            <div className='flex gap-4'>
+                {
+                    state === "login" ? "Don't have an account : " :
+                    "have an account :"
+                }
+                <button type='button' onClick={()=>{
+                    state === "login" ? setState("register"):
+                    setState("login")
+                }} className='text-blue-900 underline font-mono'>
+                {
+                    state === "login" ? "Register " :
+                    "Login"
+                }
+                </button>
+            </div>
         </form>
+
     </div> 
   )
 }
