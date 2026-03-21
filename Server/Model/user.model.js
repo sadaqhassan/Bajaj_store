0b19a0bbd08f8373393cb8f2f9e04b65e04a7d78
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-21 15:32:12 +0300

    userModel

---
+++ Server/Model/user.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const userModel = mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+},{timeStamp:true})
+
+export const theUser = mongoose.model("user",userModel);
