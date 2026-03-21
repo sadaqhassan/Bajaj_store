869d63f88d4f742371089b03fd809499bf342d09
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-21 16:01:03 +0300

    user endPoints

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import { configApp } from './utils/Config.js';
 import connectionDb from './utils/conn.js';
+import { userRoute } from './Routes/user.route.js';
 const app = express();
 
 configApp(app)
+
+app.use("/api/user",userRoute);
 
 connectionDb()
