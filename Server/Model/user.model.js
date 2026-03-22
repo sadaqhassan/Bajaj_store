5f4b4746c936fccc870a2de9786fb7d3ee72bc8a
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-22 17:13:39 +0300

    avatar

--- Server/Model/user.model.js
+++ Server/Model/user.model.js
@@ -13,6 +13,10 @@ const userModel = mongoose.Schema({
         type:String,
         required:true
     },
+    avatar:{
+        type:String,
+        default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjEofX99GPaBBLF9NPxmpiDRHAmvY00shDnw&s"
+    }
 },{timestamps:true})
 
 export const theUser = mongoose.model("user",userModel);
