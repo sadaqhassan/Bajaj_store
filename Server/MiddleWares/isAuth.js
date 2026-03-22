1f9cd3464a0da591ebb189a8c5a4aaf3fb014499
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-22 17:13:33 +0300

    AuthCheck middlewre

---
+++ Server/MiddleWares/isAuth.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+
+export const isAuthM = async (req,res,next) => {
+    try {
+        const userToken = req.cookies.accessToken;
+        if(!userToken){
+            return res.status(404).json({success:false,message:"Please login first"})
+        }
+
+        const verifyToken = jwt.verify(userToken,process.env.JWT_SECRET);
+
+        if(!verifyToken){
+            return res.status(404).json({success:false,message:"you are not autherized"})
+        };
+
+        req.userId = verifyToken.id;
+        next()
+    } catch (error) {
+        res.status(500).json({success:false,message:"server error"})
+        console.log(error)
+    }
+}
