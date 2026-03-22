6d49f3e626abee6ab9b9dff41642b8fc35b5d879
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-22 14:54:29 +0300

    toastterContainer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 import Nav from './Components/Nav'
 import Login from './Pages/Login'
+import { ToastContainer} from 'react-toastify';
+
 
 const App = () => {
   return (
     <div>
-
+      <ToastContainer/>
       <Nav/>
-
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/auth' element={<Login/>}/>
