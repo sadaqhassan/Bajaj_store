1b46e558c33731f325fdc12641fb1e74a9f1a94f
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-22 14:54:21 +0300

    register request

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,11 +1,24 @@
 import React, { useState } from 'react'
+import { toast } from 'react-toastify';
 
 const Login = () => {
     const [state,setState] = useState("login");
+    const [inputData,setInputData] = useState({});
 
-    const handleSubmit = (e)=>{
+    const handleSubmit = async(e)=>{
         e.preventDefault();
+        
+        //sendRegisterRequest
+        
+
+    const handleChange = (e)=>{
+        setInputData((prev)=>({
+            ...prev,
+            [e.target.name]:e.target.value
+        }))
     }
+
+
 
 return (
     <div className='flex flex-col justify-center items-center fixed inset-0 left-0 right-0 bottom-0 top-0 '>
@@ -15,10 +28,10 @@ return (
             }</h1>
             {
                 state === "register" &&
-                <input type="text" placeholder='username' name='username' className='bg-gray-100 px-2 py-1 rounded text-black'/>
+                <input onChange={handleChange} type="text" placeholder='username' name='username' className='bg-gray-100 px-2 py-1 rounded text-black'/>
             }
-            <input type="email" placeholder='email' name='email' className='bg-gray-100 px-2 py-1 rounded text-black'/>
-            <input type="password" placeholder='password' name='password' className='bg-gray-100 px-2 py-1 rounded text-black'/>
+            <input onChange={handleChange} type="email" placeholder='email' name='email' className='bg-gray-100 px-2 py-1 rounded text-black'/>
+            <input onChange={handleChange} type="password" placeholder='password' name='password' className='bg-gray-100 px-2 py-1 rounded text-black'/>
             <input className='bg-cyan-600 text-white py-1' type="submit" value={state === "login" ? "Login" : "Register"} />
             <div className='flex gap-4'>
                 {
