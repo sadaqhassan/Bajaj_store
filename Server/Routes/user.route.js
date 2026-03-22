ddefae02b971a4045c6f908bffe7aa12c2f24895
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-22 17:13:55 +0300

    prrofil endPoint

--- Server/Routes/user.route.js
+++ Server/Routes/user.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { loginApi, registerApi } from "../Controllers/user.controller.js";
+import { getProfileApi, loginApi, registerApi } from "../Controllers/user.controller.js";
+import { isAuthM } from "../MiddleWares/isAuth.js";
 
 const user = Router()
 
 user.post("/register",registerApi)
 user.post("/login",loginApi);
+user.get("/user-data",isAuthM,getProfileApi);
 
 export const userRoute = user;
