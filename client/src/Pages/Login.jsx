51da2929307438f8339c20cc42a898493b2428f2
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-24 12:07:50 +0300

    naviugation and fixed some err

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,33 +1,40 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 const Login = () => {
+    const navigate = useNavigate()
     const [state,setState] = useState("login");
+    const [loading,setLoading] = useState(false);
     const [inputData,setInputData] = useState({});
 
     const handleSubmit = async(e)=>{
         e.preventDefault();
+        setLoading(true)
         
         //sendRequest
-        if(state === "register"){
-            const res = await fetch('http://localhost:4000/api/user/register',{
-            method:"POST",
-            headers:{"Content-Type": "application/json"},
-            body:JSON.stringify(inputData)
-        })
-        const data = await res.json();
-        if(!data.success){
-            return toast.error(data.message)
-        }
-
-        toast.success(data.message)
-    }else{
-        const res = await fetch('http://localhost:4000/api/user/login',{
+        if(state === "login"){
+            const res = await fetch('http://localhost:4000/api/user/login',{
             method:"POST",
             headers:{"Content-Type": "application/json"},
             credentials:"include",
             body:JSON.stringify(inputData)
         })
+        const data = await res.json();
+        setLoading(false)
+        if(!data.success){
+            return toast.error(data.message)
+        }
+
+        toast.success(data.message)
+        navigate("/profile")
+    }else{
+        const res = await fetch('http://localhost:4000/api/user/register',{
+            method:"POST",
+            headers:{"Content-Type": "application/json"},
+            body:JSON.stringify(inputData)
+        })
+        setLoading(false)
         const data = await res.json();
         if(!data.success){
             return toast.error(data.message)
@@ -59,7 +66,10 @@ return (
             }
             <input onChange={handleChange} type="email" placeholder='email' name='email' className='bg-gray-100 px-2 py-1 rounded text-black'/>
             <input onChange={handleChange} type="password" placeholder='password' name='password' className='bg-gray-100 px-2 py-1 rounded text-black'/>
-            <input className='bg-cyan-600 text-white py-1' type="submit" value={state === "login" ? "Login" : "Register"} />
+            {
+                !loading ? <input className='bg-cyan-600 text-white py-1' type="submit" value={state === "login" ? "Login" : "Register"} /> 
+                : <input className='bg-cyan-600 text-white py-1' type="submit" disabled value="Loading....." />
+            }
             <div className='flex gap-4'>
                 {
                     state === "login" ? "Don't have an account : " :
