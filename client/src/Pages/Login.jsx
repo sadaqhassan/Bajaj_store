6e9539d29ddeac0aa9a490ac9fd8163b7f10cf8d
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-26 14:05:13 +0300

    loading

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -60,7 +60,7 @@ const Login = () => {
 
 
 return (
-    <div className='flex flex-col justify-center items-center fixed inset-0 left-0 right-0 bottom-0 top-0 '>
+    <div className='flex flex-col justify-center items-center mt-16 inset-0 left-0 right-0 bottom-0 top-0 '>
         <form onSubmit={handleSubmit} className='p-10 rounded bg-white shadow-2xl flex flex-col space-y-4' action="">
             <h1 className='text-center text-2xl font-medium mb-4'>{
                 state === "login" ? "Login" : "Register"
