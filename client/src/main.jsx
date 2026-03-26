a185307365109c25f6861bb73f889276a8ebdc1e
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-26 19:07:29 +0300

    main persistor

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,14 +4,17 @@ import './index.css'
 import App from './App.jsx'
 
 import { BrowserRouter } from 'react-router-dom'
-import { store } from './Store/store.js'
+import { persistor, store } from './Store/store.js'
 import { Provider } from 'react-redux'
 
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Provider store={store}>
+      <PersistGate persistor={persistor}>
       <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>,
 )
