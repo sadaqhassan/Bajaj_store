e7afe2f6be8cf6ab15fe9abd87afef0d6c6f300a
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-27 14:54:08 +0300

    profile

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -9,7 +9,7 @@ const Profile = () => {
     <div className='flex flex-col justify-center items-center mt-10'>
         <div className='bg-white shadow-2xl roundeded p-10'>
             <h1 className='text-center text-2xl font-medium mb-4'>Profile</h1>
-            <img src="./user.png" alt="" className='w-18 h-18 rounded-full mb-5 ml-15'/>
+            <img src={currentUser.avatar? currentUser.avatar : "./user.png"} alt="" className='w-18 h-18 rounded-full mb-5 ml-15'/>
             <div className='flex flex-col  space-y-4'>
             <div className='text-gray-600'> <input onChange={handleChange} type="text" defaultValue={currentUser.username} name='username' className='bg-gray-100 px-2 py-1 rounded text-black'/></div>
             <div className='text-gray-600'> <input onChange={handleChange} type="email"  defaultValue={currentUser.email} name='email' className='bg-gray-100 px-2 py-1 rounded text-black'/></div>
