905dfe3e04a4675b43641ea139447d47dd527347
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-28 19:02:41 +0300

    cenerbnner

---
+++ client/src/Components/CenterBanner.jsx
@@ -0,0 +1,34 @@
+import { ArrowBigLeft, ArrowBigRight } from 'lucide-react'
+import React, { useEffect, useState } from 'react'
+
+function CenterBanner() {
+    const [costomers,setCostommers] = useState(0)
+    
+    useEffect(()=>{
+        let start = 0
+        let end = 2878
+        
+        const interval = setInterval(() => {
+            start += 50
+
+            if(start >= end){
+                start = end
+                clearInterval(interval)
+            }
+            setCostommers(start);
+        },1000);
+    },[])
+return (
+    <div className='cone flex justify-center text-xl  py-6 items-center my-16 bg-gray-100 shadow-2xl rounded px-10 '>
+        <p className='text-xl font-bold mr-10'>
+            Customers have placed their trust in us
+        </p>
+        <ArrowBigRight/>
+        <div>
+            <p className='custom font-bold font-bol ml-10'>+{costomers}</p>
+        </div>
+    </div>
+  )
+}
+
+export default CenterBanner
