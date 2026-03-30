2286ed13c1fd1330d84afac3b291fa3361b2e30e
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-30 12:17:11 +0300

    showLsit

--- Server/Routes/bajaj.route.js
+++ Server/Routes/bajaj.route.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import { bajajPostApi, myListing } from "../Controllers/Bajaj.controller.js";
+import { isAuthM } from "../MiddleWares/isAuth.js";
 
 const bajaj = Router();
 
-bajaj.post("")
+bajaj.post("/create-bajaj",isAuthM,bajajPostApi);
+bajaj.post("/my-list",isAuthM, myListing);
 
 export const bajajRoute = bajaj
