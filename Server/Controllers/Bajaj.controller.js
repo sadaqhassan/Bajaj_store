b2d6ef0b6e82302e290c2223cd66b8a2a24c3245
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-30 12:16:50 +0300

    createLis

--- Server/Controllers/Bajaj.controller.js
+++ Server/Controllers/Bajaj.controller.js
@@ -1,10 +1,35 @@
+import { Bajaj } from "../Model/bajaj.model.js";
+
 export const bajajPostApi = async (req,res) => {
     const id = req.userId;
 
     try {
+        const listing = await Bajaj.create({
+            ...req.body,
+            assignedTo:id
+        });
+        if(!listing) return res.status(400).json({success:false,message:"error happen"})
         
+        return res.status(200).json({success:true,message:"list published"})
     } catch (error) {
         res.status(500).json({success:false,message:"server error"})
         console.log(error)
+    }
+}
+
+
+export const myListing = async (req,res) => {
+    
+    const id = req.userId
+
+    try {
+        const userListing = await Bajaj.findOne({assingnedTo:id});
+
+        return res.status(200).json({success:true,data:userListing});
+
+    } catch (error) {
+        res.status(500).json({success:false,message:"server error"})
+        console.log(error)
+    
     }
 }
