f62fce6445dc88f00f0a2fd931fe0f7b4c191f8b
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-01 16:14:05 +0300

    bajaaj Delete route

--- Server/Routes/bajaj.route.js
+++ Server/Routes/bajaj.route.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { bajajPostApi, myListing } from "../Controllers/Bajaj.controller.js";
+import { bajajPostApi, deleteList, myListing } from "../Controllers/Bajaj.controller.js";
 import { isAuthM } from "../MiddleWares/isAuth.js";
 
 const bajaj = Router();
 
 bajaj.post("/create-bajaj",isAuthM,bajajPostApi);
 bajaj.get("/my-list",isAuthM, myListing);
-bajaj.delete("/my-list",isAuthM, deleteList);
+bajaj.delete("/my-list/delete/:id",isAuthM, deleteList);
 
 export const bajajRoute = bajaj
