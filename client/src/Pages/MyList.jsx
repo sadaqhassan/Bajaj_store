32f371be2511fd8d71c11e2e8017dc3229b9c352
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-01 16:13:27 +0300

    fxed

--- client/src/Pages/MyList.jsx
+++ client/src/Pages/MyList.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { useEffect } from 'react';
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 const MyList = () => {
     const [bajaj,setBajaj] = useState([]);
+    const navigate = useNavigate()
     const [loading ,setloading] = useState(false)
 
 const getList = async () => {
@@ -19,20 +21,30 @@ const getList = async () => {
                 return toast.error(data.message);
             }
             setBajaj(data.data)
-            console.log(data.data)
             toast.success(data.message);
+    }
+
+
+
+
+    const handleDelete = async (id) => {
+        console.log(bajaj)
+        const res = await fetch(`http://localhost:4000/api/bajajs/my-list/delete/${id}`,{
+            method:"DELETE",
+            credentials:"include"
+        });
+        const data = await res.json();
+        if(!data.success){
+            return toast.error(data.message)
+        }
+
+        toast.success(data.message);
+        getList()
     }
 
     useEffect(()=>{
         getList()
     },[])
-
-    const bajajs = [
-        { id: 1, img: "https://assets.prebuiltui.com/images/components/bajaj/bajaj-houseImage1.png", title: "The Ridgewood Apartment", location: "Aspen, Colorado", price: "$2.5 M", specs1: "4 Bed", specs2: "3 Bath" },
-        { id: 2, img: "https://assets.prebuiltui.com/images/components/bajaj/bajaj-houseImage2.png", title: "The Amethyst Arbour", location: "Aspen, Colorado", price: "$3.2 M", specs1: "3 Bed", specs2: "2 Bath" },
-        { id: 3, img: "https://assets.prebuiltui.com/images/components/bajaj/bajaj-houseImage2.png", title: "The Amethyst Arbour", location: "Aspen, Colorado", price: "$3.2 M", specs1: "3 Bed", specs2: "2 Bath" },
-        { id: 4, img: "https://assets.prebuiltui.com/images/components/bajaj/bajaj-houseImage1.png", title: "The Ridgewood Apartment", location: "Aspen, Colorado", price: "$2.5 M", specs1: "4 Bed", specs2: "3 Bath" }
-    ];
 
 return !loading ?(
         <>
@@ -44,7 +56,9 @@ return !loading ?(
             </style>
             <div>
             <div className="flex flex-col items-center justify-center py-20 px-4">
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-4.5">
+                {
+                    bajaj.length > 0 ?
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4.5">
                     {bajaj.map((bajaj,key) => (
                         <div key={bajaj.id} className="flex items-center p-2 border border-black/10 hover:border-black/20 transition-colors rounded-xl w-sm sm:w-[420px]">
                             <img src={bajaj?.images[0]} alt="House" className="w-full max-w-[118px] rounded-lg object-cover" />
@@ -59,14 +73,20 @@ return !loading ?(
                                     {bajaj.model}
                                 </div>
                             <div className="text-sm mt-4  flex items-center gap-1.5">
-                                <button className='text-sm text-green-600 px-2'>update</button>    
-                                <button className='text-sm text-red-600 px-2'>delete</button>    
+                                <button className='text-sm text-green-600 px-2' >update</button>    
+                                <button className='text-sm text-red-600 px-2' onClick={()=>handleDelete(bajaj._id)}>delete</button>    
                             </div>
                             </div>
                         </div>
                         
                     ))}
                     </div>
+                    :
+                    <div className='flex flex-col justify-center  items-center mt-40'>
+                        <p>There is no list</p>
+                        <button onClick={()=>navigate('/profile/create-list')} className='bg-green-600 mt-5 px-2 py-1 rounded text-white text-white'>Create new List</button>
+                    </div> 
+                }
                 </div>
                 
             </div>
