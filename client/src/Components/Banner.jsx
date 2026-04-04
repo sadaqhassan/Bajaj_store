f15413d48d1140c39bde3668b0d8ac5ffb5cadbf
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-04 14:25:22 +0300

    dst

--- client/src/Components/Banner.jsx
+++ client/src/Components/Banner.jsx
@@ -6,6 +6,7 @@ const Banner = () => {
 return (
     <div className='Cone flex flex-col justify-center items-center mt-30 mb-10 mx-40'>
         <div className='flex flex-col md:flex-row justify-between md:gap-20 gap-6'>
+            
         <img src="./tuktuk.png" className='md:w-56 md:h-56 w-86 h-46' alt="" />
         <div className='flex-wrap flex-col  '>
         <h1 className='text-3xl md:text-6xl mb-5 font-bold'>Ku soo dhawoow <br/>Bajaj Store-keena</h1>
