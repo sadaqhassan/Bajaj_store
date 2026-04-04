0c42f630571fdbfb8c4a284de4f207381ba0fafa
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-04 13:29:03 +0300

    fxd

--- client/src/Utils/firebase.js
+++ client/src/Utils/firebase.js
@@ -8,11 +8,17 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_API_KEY,
+    
     authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+
     projectId: import.meta.env.VITE_PROJECT_ID,
+
     storageBucket: import.meta.env.VITE_STORAGE_BUDGET,
+
     messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+
     appId: import.meta.env.VITE_APP_ID,
+
     measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
