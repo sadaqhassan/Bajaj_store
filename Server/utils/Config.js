a23fbf59d419869dbe4ee40b899919b20a72aef4
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-04 14:01:47 +0300

    cors

--- Server/utils/Config.js
+++ Server/utils/Config.js
@@ -10,7 +10,7 @@ export const configApp = (app)=>{
     app.listen(PORT,()=>console.log("server is running on http://localhost:"+PORT))
     app.use(express.json());
     app.use(cors({
-        origin:"https://bajaj-store-hh9s.vercel.app",
+        origin:"https://bajajstore.vercel.app",
         credentials:true
     }))
 }  
